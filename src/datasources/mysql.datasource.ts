1e4677c891fcf3f6a75bfe066ce81a4f58c0380c
Ocup <ocup0311@gmail.com>
2024-11-07 17:10:45 +0800

    [CLEAN] remove some useless files & comments
    1. 移除一些透過 CLI 初始化生成與專案無關的檔案＆註解

--- src/datasources/mysql.datasource.ts
+++ src/datasources/mysql.datasource.ts
@@ -12,10 +12,6 @@ const config = {
   database: process.env.MYSQL_DB_NAME,
 };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class MysqlDataSource
   extends juggler.DataSource
--- src/migrate.ts
+++ src/migrate.ts
@@ -11,11 +11,6 @@ export async function migrate(args: string[]) {
   await app.boot();
   await app.migrateSchema({
     existingSchema,
-    // The order of table creation is important.
-    // A referenced table must exist before creating a
-    // foreign key constraint.
-    // For PostgreSQL connector, it does not create tables in the
-    // right order.  Therefore, this change is needed.
     models: ['Todo', 'Item'],
   });
 
